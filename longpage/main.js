eb7b3eeeec5047b3c2580708b17ff684e5f155f7
Jasmine <winter4439@gmail.com>
2017-03-18 06:06:57 -0700

    Add files via upload

--- longpage/main.js
+++ longpage/main.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
-
-  console.log("oh glorious master, your document is read!");
-
-  var fromTop = 0;
-
   $(window).scroll(function(){
+    var fromTop = $(window).scrollTop();
+    console.log(fromTop);
 
-   fromTop = $(window).scrollTop();
+  $("#distance").text("Here is " + fromTop +"px");
 
-   console.log("you are at" + fromTop + "px");
+  $("#distance").css({
+  "top" : 0 + fromTop * 1 +"px",
+  "font-size": "20px",
+  "padding-top":"20px",
+  });
 
-     })//close .scroll
-   });//close .ready
+$("#circle").css({"top" : 0 + fromTop * 1 + "px" });
+
+if(fromTop > 250){
+  $("#person1").css({
+  "display":"none"});
+  }
+
+else {
+  $("#person1").css({
+  "top" : 240 + fromTop * 1 + "px",
+  "display":"block"});
+  }
+  if(fromTop > 1280){
+    $("#cartop").css({
+    "display":"none"});
+    }
+
+  else {
+    $("#cartop").css({
+    "top" : 500 + fromTop * 1 + "px",
+    "display":"block"});
+    }
+  });//end of scroll
+  });//end of .ready function
